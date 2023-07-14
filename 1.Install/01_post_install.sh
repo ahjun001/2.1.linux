576f219359b797f9dcc85ddee073e72759682dba
ahjun001 <sudo.ubuntu@disroot.org>
2023-07-14 12:10:29 +0800

    remove unecessary comments

--- 1.Install/01_post_install.sh
+++ 1.Install/01_post_install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# shellcheck disable=
 
 # post install script to install favorite environment, apps, and settings
 
@@ -81,5 +80,5 @@ bash ./02_httrack.sh
 
 # install printer driver
 bash ./02_install_printer_driver.sh
- 
+
 cat "$INSTALL_LOG"
