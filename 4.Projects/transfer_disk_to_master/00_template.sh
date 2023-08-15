4c7d12078b71ebab5f82b0645fbae5a99a413b05
ahjun001 <sudo.ubuntu@disroot.org>
2023-08-15 14:57:31 +0800

    new version

--- 4.Projects/transfer_disk_to_master/00_template.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# shellcheck source=/dev/null
-. ./01_commons.sh
-$DBG $'\n'"$(basename "${BASH_SOURCE[0]}")"$'\n'
--- 4.Projects/transfer_disk_to_master/03a_erase_non_canonically_installed_dirs.sh
+++ 4.Projects/transfer_disk_to_master/03a_erase_non_canonically_installed_dirs.sh
@@ -19,18 +19,3 @@ delDirs=(
 for dir in "${delDirs[@]}"; do
     find "$DISK" -type d -name "$dir" -print0 | xargs -0r sudo rm -r 
 done
-
-# sudo find "$DISK" -type d -name '.git' -print0 | xargs -0 rm -r 
-# sudo find "$DISK" -type d -name '.idea' -print0 | xargs -0 rm -r 
-
-# First was confirming before deletion, .git dirs require supersuser priviledges
-# find all dirs and put in a list
-# my_l=()
-
-# for dir in "${delDirs[@]}"; do
-#     while IFS= read -r -d '' f; do
-#         my_l+=("$f")
-#     done < <(find "$DISK" -type d -name "$dir" -print0)
-# done
-
-# Erase_my_l_or_exit 'non_canonically_installed_dirs'
