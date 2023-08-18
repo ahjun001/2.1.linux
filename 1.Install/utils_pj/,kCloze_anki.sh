cd269ac79bd729923cc344ef1f384309a0409c3c
ahjun001 <todo.ubuntu@gmail.com>
2023-08-18 12:45:03 +0800

    add Anki Cloze & clean

---
+++ 1.Install/utils_pj/,kCloze_anki.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+xclip -o |
+    sed '/\[sound:/s/ / ::/' |
+    sed '/\[sound:/s/^/{{c::/' |
+    sed -e '/\[sound:/{N;N;s/$/\}\}/}' |
+    xclip -i -selection clipboard
+
+[[ -S /usr/local/sbin/,kCloze_anki.sh ]] || sudo ln -fs ~/Documents/Github/2.1.linux/1.Install/utils_pj/,kCloze_anki.sh /usr/local/sbin/,kCloze_anki.sh
---
+++ 1.Install/utils_pj/,kclean_anki.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+xclip -o |
+    sed 's/{{c.:://g' |
+    sed 's/:://g' |
+    sed 's/}}//g' |
+    xclip -i -selection clipboard
+
+[[ -S /usr/local/sbin/,kclean_anki.sh ]] || sudo ln -fs ~/Documents/Github/2.1.linux/1.Install/utils_pj/,kclean_anki.sh /usr/local/sbin/,kclean_anki.sh
