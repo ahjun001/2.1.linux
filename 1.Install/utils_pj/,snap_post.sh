1f3b95c8e24457a03c15b8a2fc76dc7abf5b62d8
ahjun001 <sudo.ubuntu@disroot.org>
2023-12-19 15:27:17 +0800

    make snap macros & encapsulators foolproof

--- 1.Install/utils_pj/,snap_post.sh
+++ 1.Install/utils_pj/,snap_post.sh
@@ -2,35 +2,67 @@
 
 # set -euo pipefail
 
-echo "Latest relevant environment variables can be set with:"
-echo "source /tmp/snapper/pj_snap.sh"
-
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     echo "${0##*/}" $'only can be sourced\nExiting.'
     exit 1
 fi
 
-# Run the snapper command and save the output number in PJ_SNAP_HOME_POST
-PJ_SNAP_HOME_POST=$(sudo snapper -c home create --type post --pre-number "$PJ_SNAP_HOME_PRE" --description "$PJ_SNAP_DESCRIPTION" --cleanup-algorithm number --print-number)
-export PJ_SNAP_HOME_POST
+cat <<.
+    PJ_SNAP_DESCRIPTION = "$PJ_SNAP_DESCRIPTION"
+    PJ_SNAP_ROOT_POST = "$PJ_SNAP_ROOT_POST"
+    PJ_SNAP_HOME_POST = "$PJ_SNAP_HOME_POST"
 
-# Run the snapper command and save the output number in PJ_SNAP_ROOT_POST
-PJ_SNAP_ROOT_POST=$(sudo snapper -c root create --type post --pre-number "$PJ_SNAP_ROOT_PRE" --description "$PJ_SNAP_DESCRIPTION" --cleanup-algorithm number --print-number)
-export PJ_SNAP_ROOT_POST
+    Latest relevant environment variables can be set with:
+    source /tmp/snapper/pj_snap.sh"
+
+    Filters can be set with
+    $ sudo -E nvim /usr/snapper/filters/excluded.txt
+
+.
+
+# Function to ask the user if they want to run a command
+ask_and_run() {
+    local prompt="$1"
+    local cmd="$2"
+    local varname="$3"
+
+    read -rp "$prompt [yY] "
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        export "$varname"="$(eval "$cmd")"
+    fi
+}
+
+# Ask if the user wants to run the snapper command and save the output number in PJ_SNAP_HOME_POST
+ask_and_run "Do you want to run 'sudo snapper -c home create --type post --pre-number '$PJ_SNAP_HOME_PRE' --description '$PJ_SNAP_DESCRIPTION'?" \
+    "sudo snapper -c home create --type post --pre-number \"$PJ_SNAP_HOME_PRE\" --description \"$PJ_SNAP_DESCRIPTION\" --cleanup-algorithm number --print-number" \
+    "PJ_SNAP_HOME_POST"
+
+# Ask if the user wants to run the snapper command and save the output number in PJ_SNAP_ROOT_POST
+ask_and_run "Do you want to run 'sudo snapper -c root create --type post --pre-number '$PJ_SNAP_ROOT_PRE' --description '$PJ_SNAP_DESCRIPTION'?" \
+    "sudo snapper -c root create --type post --pre-number \"$PJ_SNAP_ROOT_PRE\" --description \"$PJ_SNAP_DESCRIPTION\" --cleanup-algorithm number --print-number" \
+    "PJ_SNAP_ROOT_POST"
+
+# Function to ask the user if they want to run a status command
+ask_and_run_status() {
+    local prompt="$1"
+    local cmd="$2"
+    local filename="$3"
+
+    read -rp "$prompt [yY] "
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        eval "$cmd" | sudo tee "/tmp/snapper/$filename.txt" 
+    fi
+}
 
 # Ask if the user wants to run the status command for home
-read -rn 1 -p "Do you want to run 'sudo snapper -c home status $PJ_SNAP_HOME_PRE..$PJ_SNAP_HOME_POST'? (y/n) "
-case $REPLY in y | Y)
-    sudo snapper -c home status "$PJ_SNAP_HOME_PRE".."$PJ_SNAP_HOME_POST"
-    ;;
-esac
+ask_and_run_status "Do you want to run 'sudo snapper -c home status $PJ_SNAP_HOME_PRE..$PJ_SNAP_HOME_POST'?" \
+    "sudo snapper -c home status \"$PJ_SNAP_HOME_PRE\"..\"$PJ_SNAP_HOME_POST\"" \
+    "$PJ_SNAP_HOME_PRE..$PJ_SNAP_HOME_POST"
 
 # Ask if the user wants to run the status command for root
-read -rn 1 -p "Do you want to run 'sudo snapper -c root status $PJ_SNAP_ROOT_PRE..$PJ_SNAP_ROOT_POST'? (y/n) "
-case $REPLY in y | Y)
-    sudo snapper -c root status "$PJ_SNAP_ROOT_PRE".."$PJ_SNAP_ROOT_POST"
-    ;;
-esac
+ask_and_run_status "Do you want to run 'sudo snapper -c root status $PJ_SNAP_ROOT_PRE..$PJ_SNAP_ROOT_POST'?" \
+    "sudo snapper -c root status \"$PJ_SNAP_ROOT_PRE\"..\"$PJ_SNAP_ROOT_POST\"" \
+    "$PJ_SNAP_ROOT_PRE..$PJ_SNAP_ROOT_POST"
 
 # make a encapsulation in /usr/local/sbin to use in $PATH
 
@@ -38,8 +70,9 @@ FILE_IN_PATH=/usr/local/sbin/,snap_post.sh
 if [[ ! -f $FILE_IN_PATH ]]; then
     cat <<. | sudo tee "$FILE_IN_PATH" >/dev/null
 #!/usr/bin/env bash
+# /usr/local/sbin/,snap_post.sh
 
-set -euo pipefail
+# set -euo pipefail
 
 # shellcheck disable=SC1091
 source /home/perubu/Documents/Github/2.1.linux/1.Install/utils_pj/,snap_post.sh
--- 1.Install/utils_pj/,snap_pre.sh
+++ 1.Install/utils_pj/,snap_pre.sh
@@ -2,18 +2,19 @@
 # ,snap_pre.sh
 
 set -o pipefail # set -e not good because will get the terminal window to exit when script is sourced
+
 # set u will cause prompt definition in inside shell to fail as well and script to exit
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo -e "\n${0##*/}" $'only can be sourced\nExiting.\n'
+    exit 1
+fi
 
 # Check if there is one and only one argument
 [[ "$#" -ne 1 ]] && {
-    echo "Usage: source ${0##*/} <description>"
-    return 1
+    echo -e "\nUsage: source ${0##*/} <description>\n"
+    if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then exit 1; else return 1; fi
 }
-
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    echo "${0##*/}" $'only can be sourced\nExiting.'
-    exit 1
-fi
 
 # Save the description in PJ_SNAP_DESCRIPTION
 PJ_SNAP_DESCRIPTION=$1
@@ -29,16 +30,31 @@ export PJ_SNAP_ROOT_PRE
 
 # save snapshot values so that they can be used in a different terminal window if needed
 mkdir -p /tmp/snapper
-cat <<. >/tmp/snapper/pj_snap.sh
+cat <<. >/tmp/snapper/snap_pre_post_env.sh
 #!/usr/bin/env bash
-if "${BASH_SOURCE[0]}" == "${0}" ; then
-    echo "${0##*/}" $'only can be sourced\nExiting.'
-    return 1
+# /tmp/snapper/snap_pre_post_env.sh
+
+if [[ "\${BASH_SOURCE[0]}" == "\${0}" ]]; then
+    cat <<EOF
+
+/tmp/snapper/,snap_pre_post_env.sh can only be sourced
+Exiting.
+
+EOF
+    exit 1
 fi
+
 export PJ_SNAP_DESCRIPTION="$PJ_SNAP_DESCRIPTION"
 export PJ_SNAP_HOME_PRE=$PJ_SNAP_HOME_PRE
 export PJ_SNAP_ROOT_PRE=$PJ_SNAP_ROOT_PRE
+
 .
+
+# echo PJ_SNAP_DESCRIPTION = "$PJ_SNAP_DESCRIPTION"
+# echo PJ_SNAP_HOME = "$PJ_SNAP_HOME_PRE"
+# echo PJ_SNAP_ROOT = "$PJ_SNAP_ROOT_PRE"
+sudo snapper -c root list | grep "$PJ_SNAP_DESCRIPTION"
+sudo snapper -c home list | grep "$PJ_SNAP_DESCRIPTION"
 
 # make a encapsulation in /usr/local/sbin to use in $PATH
 
@@ -46,8 +62,20 @@ FILE_IN_PATH=/usr/local/sbin/,snap_pre.sh
 if [[ ! -f $FILE_IN_PATH ]]; then
     cat <<. | sudo tee "$FILE_IN_PATH" >/dev/null
 #!/usr/bin/env bash
+# /usr/local/sbin/,snap_pre.sh
 
-set -euo pipefail
+# set -euo pipefail
+
+if [[ \${BASH_SOURCE[0]} == "\${0}" ]] ; then
+    echo "/tmp/snapper/,snap_pre.sh" \$'only can be sourced\nExiting.'
+    exit 1
+fi
+
+# Check if there is one and only one argument
+[[ "\$#" -ne 1 ]] && {
+    echo -e "\nUsage: source \${0##*/} <description>\n"
+    return 1
+}
 
 # shellcheck disable=SC1091
 source /home/perubu/Documents/Github/2.1.linux/1.Install/utils_pj/,snap_pre.sh "\$1"
